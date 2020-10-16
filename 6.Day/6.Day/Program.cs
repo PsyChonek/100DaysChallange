654ce55418cbaeca8fee370eb3bbdbc06f8fd595
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-16 17:10:00 +0200

    Day 6

---
+++ 6.Day/6.Day/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            RunningSum(nums);
+            foreach(int number in nums)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        static int[] RunningSum(int[] nums)
+        {
+            for (int P = 1; P < nums.Length; P++)
+            {             
+                    nums[P] += nums[P - 1];                
+            }
+            return nums;
+        }
+        
+
+
+    }
+}
