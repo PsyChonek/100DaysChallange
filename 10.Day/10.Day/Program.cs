f174da5526b012602e79f27d975ca44cfe0a3a63
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-20 20:59:51 +0200

    Day 10

---
+++ 10.Day/10.Day/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _10.Dayd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 14;
+            Console.WriteLine(NumberOfSteps(input));
+
+        }
+        static int NumberOfSteps(int num)
+        {
+            int output = 0;
+
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num--;
+                }
+                output++;
+            }
+            return output;
+        }
+    }
+}
