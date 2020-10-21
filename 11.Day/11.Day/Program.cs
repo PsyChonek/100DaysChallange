8b93db063525e102e460630c9c708fe6db3e1e1d
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-21 17:08:53 +0200

    Day 11

---
+++ 11.Day/11.Day/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _11.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "codeleet";
+            int[] indicates = {4, 5, 6, 7, 0, 2, 1, 3};
+            Console.WriteLine(RestoreString(s,indicates));
+        }
+
+        static string RestoreString(string s, int[] indices)
+        {   
+            StringBuilder sb = new StringBuilder(s);
+            int P = 0;
+
+            foreach (int I in indices)
+            {              
+                sb[I] = s[P];
+                P++;
+            }
+            return sb.ToString();
+        }
+    }
+}
