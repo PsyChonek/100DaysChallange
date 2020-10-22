8a64bab213dc1cc1be838164671f89ed9ace1956
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-22 10:09:42 +0200

    Day 12

---
+++ 12.Day/12.Day/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] nums = {8, 1, 2, 2, 3};
+
+            foreach (int P in SmallerNumbersThanCurrent(nums))
+            {
+                Console.WriteLine(P);
+            }
+
+        }
+        static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            List<int> result = new List<int>();
+            
+            for (int P = 0; P < nums.Length; P++)
+            {
+                int temp = 0;
+                foreach (int num in nums)
+                {
+                    if (nums[P] > num)
+                    {                       
+                        temp++;
+                    }
+                }
+                result.Add(temp);
+            }
+            return result.ToArray();
+        }
+    }
+}
