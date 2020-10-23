4a06097826f0100d45e7047b38d35bb01c76c1d4
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-23 11:49:03 +0200

    Day 13

---
+++ 13.Day/13.Day/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace _13.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 234;
+            Console.WriteLine(SubtractProductAndSum(n));
+        }
+
+        static int SubtractProductAndSum(int n)
+        {
+            int product = 1;
+            int sum = 0;
+            while (n > 0)
+            {
+                int num = n % 10;
+                product *= num;
+                sum += num;
+                n /= 10;
+            }
+            return product-sum;
+        }
+    }
+}
