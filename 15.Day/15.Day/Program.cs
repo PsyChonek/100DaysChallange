a824653e984cebb7eb5fb5ba47b043c01c4a9d61
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-25 18:18:28 +0100

    Day 15

---
+++ 15.Day/15.Day/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _15.Day
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int big = 1;
+            int medium = 1;
+            int small = 0;
+            int[] cars = {1, 2, 3, 1};
+
+            ParkingSystem obj = new ParkingSystem(big, medium, small);
+
+            foreach (int carType in cars)
+            {
+                Console.WriteLine(obj.AddCar(carType));
+            }
+        }
+    }
+
+    public class ParkingSystem
+    {
+        int[] p;
+    public ParkingSystem(int big, int medium, int small)
+        {
+        p = new int[] { big, medium, small };
+        }
+
+        public bool AddCar(int carType)
+        {
+            return --p[carType - 1] >= 0; 
+        }
+    }
+
+    /**
+     * Your ParkingSystem object will be instantiated and called as such:
+     * ParkingSystem obj = new ParkingSystem(big, medium, small);
+     * bool param_1 = obj.AddCar(carType);
+     */
+}
