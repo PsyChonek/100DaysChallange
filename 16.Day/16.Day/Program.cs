019d284a12b2ab915a678fe2b6b61f21f7562790
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-26 12:28:05 +0100

    Day 16

---
+++ 16.Day/16.Day/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _16.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "1+(2*3)/(2-1)";
+            Console.WriteLine(MaxDepth(test));
+        }
+
+        static int MaxDepth(string s)
+        {
+            int depth = 0;
+            int temp = 0;
+
+            for (int P = 0; P < s.Length; P++)
+            {
+                if (s[P] == '(')
+                {
+                    temp++;
+                }
+                else if (s[P] == ')')
+                {
+                    temp--;
+                }
+
+                if (depth < temp)
+                {
+                    depth = temp;
+                }
+            }
+
+            return depth;
+        }
+
+    }
+
+}
