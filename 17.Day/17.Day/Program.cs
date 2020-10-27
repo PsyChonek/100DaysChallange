239d3d81de577f530997c6021ce62dee4604ce39
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-27 13:04:15 +0100

    Day 17

---
+++ 17.Day/17.Day/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace _17.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 2, 3, 4 };
+            int[] index = { 0, 1, 2, 2, 1 };
+
+            foreach (int V in CreateTargetArray(nums, index))
+            {
+                Console.WriteLine(V);
+            }
+
+        }
+        static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            List<int> result = new List<int>();
+
+            for (int P = 0; P < nums.Length; P++)
+            {
+                result.Insert(index[P], nums[P]);
+            }
+            return result.ToArray();
+
+
+
+        }
+    }
+}
