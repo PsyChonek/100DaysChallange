71ed990da15362093a8800dac12841cb74a33813
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-28 13:53:33 +0100

    Day 18

---
+++ 18.Day/18.Day/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _18.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, start = 7;
+
+            Console.WriteLine(XorOperation(n, start));
+
+
+        }
+
+        static int XorOperation(int n, int start)
+        {
+            int Temp = start;
+
+            for (int P = 1; P < n; P++)
+            {
+                Temp = Temp ^ start + 2 * P;
+            }
+            return Temp;
+        }
+    }
+}
