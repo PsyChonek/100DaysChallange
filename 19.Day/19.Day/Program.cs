1f81c3dde3661426da6f4c58ac8e3fe31ce3c7b6
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-29 15:16:35 +0100

    Day 19

---
+++ 19.Day/19.Day/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _19.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "RLRRRLLRLL";
+            Console.WriteLine(BalancedStringSplit(input));
+        }
+        static int BalancedStringSplit(string s)
+        {
+            int result = 0;
+            int temp = 0;
+            for (int P = 0; P < s.Length; P++)
+            {
+                if (s[P] == 'L')
+                {
+                    temp++;
+                }
+                else
+                {
+                    temp--;
+                }
+                if (temp == 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
