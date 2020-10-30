09d52ea31e568cfdcfd80fb58e3dc34e985e8a50
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-30 12:03:10 +0100

    Day 20

---
+++ 20.Day/20.Day/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _20.Day
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 11, 12 };
+            Console.WriteLine(SumOddLengthSubarrays(arr));    
+        }
+
+        static int SumOddLengthSubarrays(int[] arr)
+        {
+            int sum = 0;
+            
+            for (int N = 0; N <= arr.Length; N += 2)
+            {
+                for (int P = 0; P < arr.Length-N; P++)
+                {
+                    for (int T = 0; T <= N; T++)
+                    {
+                        sum += arr[P + T];
+                    }
+                }
+            }
+
+            return sum;   
+        }
+
+    }
+
+
+}
