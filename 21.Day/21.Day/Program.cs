6062c1ab269fcff6282342056d52a37d483dd5f6
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-31 12:37:25 +0100

    21 Day

---
+++ 21.Day/21.Day/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace _21.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 12, 345, 2, 6, 7896};    
+            Console.WriteLine(FindNumbers(nums));
+            // To.String(); Better 
+        }
+
+        static int FindNumbers(int[] nums)
+        {
+            int result = 0;
+
+            for (int P = 0; P<nums.Length; P++)
+            {
+                List<int> num = new List<int>();
+                int temp = nums[P];
+                while (temp >= 10)
+                {
+                    num.Add(temp % 10);
+                    temp /= 10;
+                }
+                num.Add(temp);
+
+                if (num.Count % 2 == 0)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
