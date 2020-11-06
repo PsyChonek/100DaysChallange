74df3fae1159d6f20cdd5422bcd4337f7fbe601c
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-06 20:48:34 +0100

    Day 27

---
+++ 27.Day/27.Day/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace _27.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,5,4,5 };
+            Console.WriteLine(MaxProduct(nums));
+        }
+
+        static int MaxProduct(int[] nums)
+        {
+            int Max = int.MinValue;
+
+            for (int J = 0; J < nums.Length; J++)
+            {
+                for (int I = 0; I < nums.Length; I++)
+                {
+                    if ((nums[J] - 1) * (nums[I] - 1) > Max && I != J)
+                    {
+                        Max = (nums[J] - 1) * (nums[I] - 1);
+                    }
+                }
+            }
+            return Max;
+            
+        }
+    }
+}
