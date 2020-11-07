8bbaf81d6489a62587fed95bb34f0932d998cf44
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-07 20:54:41 +0100

    Day 28

---
+++ 28.Day/28.Day/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _28.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] startTime = { 1, 2, 3 };
+            int[] endTime = { 3, 2, 7 };
+            int queryTime = 4;
+            Console.WriteLine(BusyStudent(startTime, endTime, queryTime));
+        }
+        static int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int result = 0;
+            for (int P = 0; P < startTime.Length; P++)
+            {
+                if (startTime[P] <= queryTime && queryTime <= endTime[P])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
