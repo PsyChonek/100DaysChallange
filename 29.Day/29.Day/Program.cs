dcf7c8890cc5794ea5983cd042f9f5328447f8e7
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-08 13:24:39 +0100

    Day 29

---
+++ 29.Day/29.Day/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _29.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            foreach (int I in SumZero(n))
+            {
+                Console.WriteLine(I);
+            }
+            
+        }
+        static int[] SumZero(int n)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int P = n; P > 0; P--)
+            {
+                if (P % 2 == 0)
+                {
+                    numbers.Add(P);
+                    numbers.Add(P * -1);
+                    P--;
+                }
+                else
+                {
+                    numbers.Add(0);
+                }
+            }
+
+            return numbers.ToArray();
+        }
+    }
+}
