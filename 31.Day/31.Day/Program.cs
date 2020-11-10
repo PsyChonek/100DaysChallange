b18196676f6df49ef4c43e38fde8fe0b953bc551
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-10 11:57:32 +0100

    Day 31

---
+++ 31.Day/31.Day/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _31.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            Console.WriteLine(GenerateTheString(n));
+        }
+        static string GenerateTheString(int n)
+        {
+            var builder = new StringBuilder();
+
+            for (int P = 0; P<n; P++)
+            {
+                if (n % 2 == 0)
+                {                                
+                    while (P != n-1)
+                    {
+                        builder.Append('a');
+                        P++;
+                    }
+                    builder.Append('b');
+                }
+                else
+                {
+                    while (P != n)
+                    {
+                        builder.Append('a');
+                        P++;
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
