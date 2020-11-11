9d7d586a441e603a30049d83445242cede54f595
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-11 14:13:14 +0100

    Day 32

---
+++ 32.Day/32.Day/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _32.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid =
+            {
+              new int[] {4,3,2,-1},
+              new int[] { 3,2,1,-1 },
+              new int[] { 1, 1, -1, -2 },
+              new int[] { -1, -1, -2, -3 }
+            };
+
+            Console.WriteLine(CountNegatives(grid));
+        }
+
+        static int CountNegatives(int[][] grid)
+        {
+            int result = 0;
+
+            for ( int P = 0; P < grid.Length; P++)
+            {
+                foreach (int I in grid[P])
+                {
+                    if (I < 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
