5c6737202cf61b7ee49e4c461ecd95f89045b407
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-12 10:42:38 +0100

    Day 33

---
+++ 33.Day/33.Day/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _33.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int left = 1, right = 22;
+
+
+            foreach (int I in SelfDividingNumbers(left, right))
+            {
+                Console.WriteLine(I);
+            }
+        }
+
+        static IList<int> SelfDividingNumbers(int left, int right)
+        {
+            IList<int> result = new List<int>();
+
+            for (int P = left; P <= right; P++)
+            {
+                string test = P.ToString();
+                Boolean check = true;
+
+                foreach (char T in test)
+                {                 
+                    if (T == '0' || P % int.Parse(T.ToString()) != 0)
+                    {
+                        check = false;
+                    }
+                }
+
+                if (check)
+                {
+                    result.Add(P);
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
