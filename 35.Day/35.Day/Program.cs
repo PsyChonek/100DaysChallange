04e28f9a8da30d6edd656d2343a4dfdabee0d380
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-14 11:45:08 +0100

    Day 35

---
+++ 35.Day/35.Day/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _35.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 2, 3, 3 };
+            Console.WriteLine(RepeatedNTimes(A));
+        }
+        static int RepeatedNTimes(int[] A)
+        {
+            for (int P = 0; P<A.Length;P++)
+            {
+                for (int T = P+1; T<A.Length; T++)
+                {
+                    if (A[P] == A[T])
+                    {
+                        return A[P];
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
