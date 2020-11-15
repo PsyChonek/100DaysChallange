acbf2aff1f2986ad3df49d53ae97d8869809dd45
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-15 19:53:33 +0100

    Day 36

---
+++ 36.Day/36.Day/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _36.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string moves = "LDRRLRUULR";
+            Console.WriteLine(JudgeCircle(moves));
+        }
+        static bool JudgeCircle(string moves)
+        {
+            int[] position = { 0, 0 };
+
+            foreach (char T in moves)
+            {
+                if (T == 'U')
+                {
+                    position[1]++;
+                }
+                if (T == 'D')
+                {
+                    position[1]--;
+                }
+                if (T == 'R')
+                {
+                    position[0]++;
+                }
+                if (T == 'L')
+                {
+                    position[0]--;
+                }
+            }
+
+            if (position[0] == 0 && position[1] == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
