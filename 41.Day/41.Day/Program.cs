2f31df343f718f37026f664a8956cb206b03fab2
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-20 16:16:25 +0100

    Day 41

---
+++ 41.Day/41.Day/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _41.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 24, 69, 100, 99, 79, 78, 67, 36, 26, 19 };
+            Console.WriteLine(PeakIndexInMountainArray(input));
+        }
+        static int PeakIndexInMountainArray(int[] arr)
+        {
+            int result = 0;
+            while (arr[result] < arr[result + 1])
+            result++;
+            return result;
+        }
+    }
+}
