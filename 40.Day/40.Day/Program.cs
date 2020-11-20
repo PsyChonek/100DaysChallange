186a4ca3b4aaa16ddb9d49f8017cd8b424357900
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-20 15:59:43 +0100

    Day 40

---
+++ 40.Day/40.Day/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _40.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { -4, -1, 0, 3, 10 };
+            foreach (int T in SortedSquares(input))
+            Console.WriteLine(T);
+        }
+        static int[] SortedSquares(int[] A)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int I in A)
+            {
+                result.Add(Convert.ToInt32(Math.Pow(I, 2)));
+            }
+            result.Sort();
+            return result.ToArray();
+        }
+    }
+}
