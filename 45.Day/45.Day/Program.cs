e6f89865464c4125105755c36a6fae790abd14c9
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-11-25 15:57:27 +0100

    Day 45

---
+++ 45.Day/45.Day/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _45.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = {1,10,10,10,19};
+            Console.WriteLine(CanMakeArithmeticProgression(input));
+        }
+        static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            int dif = arr[0] - arr[1];
+            for (int P = 0; P < arr.Length-1; P++)
+            {
+                if (arr[P] - arr[P+1] != dif)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
