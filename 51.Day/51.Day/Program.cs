c741f571550f03d86c0825d3fbb4b2d0689dcb48
PsyChonek <namraza159@gmail.com>
2020-11-30 08:58:03 +0100

    Day 51

---
+++ 51.Day/51.Day/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 8000, 9000, 2000, 3000, 6000, 1000 };
+            Console.WriteLine(Average(input));
+
+        }
+        static double Average(int[] salary)
+        {
+            double result = 0;
+            Array.Sort(salary);
+
+            for (int P = 1; P < salary.Length - 1; P++)
+            {
+                result += salary[P];
+            }
+            result /= salary.Length - 2;
+            return result;
+        }
+    }
+}
