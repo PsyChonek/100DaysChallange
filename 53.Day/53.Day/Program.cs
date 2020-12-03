7de22b65114ff7c62d6ded6a17629ec6c397db36
PsyChonek <namraza159@gmail.com>
2020-12-03 12:55:15 +0100

    Day 53

---
+++ 53.Day/53.Day/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _53.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "loveleetcode";
+            char C = 'e';
+
+             foreach (int I in ShortestToChar(S, C))
+               Console.Write(0);
+        }
+        static int[] ShortestToChar(string S, char C)
+        {
+            List<int> output = new List<int>();
+            bool hasBeen = false;
+            for (int P = 0; P < S.Length; P++)
+            {
+
+                int distance = -1;
+
+                for (int K = 0; K < S.Length; K++)
+                {
+                    distance++;
+                    if (hasBeen)
+                    {
+                        Console.WriteLine(K - distance);
+                        if (S[K+distance] == C || S[K-distance] == C)
+                        {
+                            Console.WriteLine("New: " + distance);
+                            break;
+                        }
+                    }
+                    else if (S[K] == C)
+                    {
+                        Console.WriteLine("Old: " + distance);
+                        if (distance == 0)
+                        {
+                            hasBeen = true;
+                        }
+                        break;
+                    }
+
+
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
