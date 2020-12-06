ff9db1f75dcf2dcc88800fbb2e33cca7c5295c09
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-12-06 11:04:52 +0100

    Day 56

---
+++ 56.Day/56.Day/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _56.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 4;
+
+            Console.WriteLine(Fib(input));
+        }
+        static int Fib(int N)
+        {
+            int second = 0;
+            int first = 1;
+            int output = 0;
+
+            if (N > 0)
+            {
+                output++;
+                for (int P = 1; P <= N; P++)
+                {
+                    output = second + first;
+                    first = second;
+                    second = output;
+
+                }
+            }
+
+
+
+            return output;
+                
+        }
+    }
+}
