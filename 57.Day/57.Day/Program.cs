1b283ca162773b6335f23ad5c70efff2c9a6f4f3
PsyChonek <namraza159@gmail.com>
2020-12-07 08:57:12 +0100

    Day 57

---
+++ 57.Day/57.Day/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 1, 2, 2, 2, 3 };
+
+            foreach (int I in FrequencySort(input))
+                Console.WriteLine(I);
+        }
+        static int[] FrequencySort(int[] nums)
+        {
+            Array.Sort(nums);
+
+            List<int> number = new List<int>();
+            List<int> times = new List<int>();
+
+            foreach (int I in nums)
+            {
+                if (number.Contains(I) == false)
+                {
+                    number.Add(I);
+                }
+            }
+
+            for (int P = 0; P < number.Count; P++)
+            {
+                int temp = 0;
+                for (int I = 0; I < nums.Length; I++)
+                {
+                    if (number[P] == nums[I])
+                    {
+                        temp++;
+                    }
+                }
+                times.Add(temp);
+            }
+
+
+
+            return times.ToArray();
+
+
+
+        }
+    }
+}
