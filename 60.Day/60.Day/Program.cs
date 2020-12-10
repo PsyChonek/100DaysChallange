e7e5eb65aabec2b5558acee29100534c9d86e321
PsyChonek <namraza159@gmail.com>
2020-12-10 12:51:23 +0100

    Day 60

---
+++ 60.Day/60.Day/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _60.Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 0, 0, 0, 1, 2, 2, 2, 3, 5, 5, 5, 5, 6, 6, 7, 7, 8, 8, 10 };
+
+            Console.WriteLine(TrimMean(arr));
+            
+        }
+
+        static double TrimMean(int[] arr)
+        {
+            Array.Sort(arr);
+
+            int i = arr.Length * 5 / 100;
+            int j = arr.Length - i;
+
+            double val = 0;
+
+            for (int k = i; k < j; k++) 
+            { 
+                val += arr[k];
+            }
+
+
+            return (i >= j) ? 0 : val / (j - i);
+        }
+    }
+}
---
+++ 60.Day/60.Day/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("60.Day")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("60.Day")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("f9e0b257-c0f8-4467-a6cf-e651ee8b6741")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
